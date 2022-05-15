ee72b2a2f4e66aa90ecdfb2e5f0001f99ea92c42
shayan <shayanejaz0000@gmail.com>
2022-05-15 12:39:03 -0700

    v4

---
+++ src/components/PLaylist/Playlist.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import "./Playlist.css";
+
+import TrackList from "../TrackList/TrackList";
+
+class Playlist  extends React.Component{
+
+    constructor(props){
+        super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+    
+    }
+
+    handleNameChange(event){
+        this.props.onNameChange(event.target.value)
+    }
+
+
+    
+ render(){
+
+    return(
+        <div className="PlayList">
+            <input onChange={this.handleNameChange} defaultValue = {'New Playlist'} />
+            <TrackList track= {this.props.PlayListTracks}
+                        isRemoval= {true}
+                        onRemove = {this.props.onRemove} />
+
+            <button className="Playlist-save" onClick= {this.props.onSave}> Save to Spotify</button>
+        </div>
+    );
+
+ }   
+    
+}
+export default Playlist;
